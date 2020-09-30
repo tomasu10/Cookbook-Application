38e7ca0a43303f67c32fea93f0ebf11bc8af29d0
tomasu10 <tomasu1027@gmail.com>
2020-09-29 23:30:43 -0400

    Fix image issue

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -14,7 +14,6 @@ export default class Recipe{
             this.author = res.data.recipe.publisher;
             this.img = res.data.recipe.image_url;
             this.url = res.data.recipe.source_url;
-            // this.url = "#"
             this.ingredients = res.data.recipe.ingredients;
         }catch(error){
             alert('Error processing recipe'); 
